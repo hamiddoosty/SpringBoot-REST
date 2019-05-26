cdd4afe8275f2e03b4266880757a87ec0a05834c
Hamid <doosty.hamid@chmail.ir>
2019-05-26 15:11:10 +0430

    Add files via upload

---
+++ src/main/java/com/tutorialspoint/RestReq/RestReqApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint.RestReq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestReqApplication.class, args);
+	}
+
+}
